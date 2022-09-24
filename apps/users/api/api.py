097396f001c0a3ed245a719bea792cc1aca7899c
Edivaldo Gomes <edslavianov@gmail.com>
2022-09-25 01:42:56 +0200

    Use Decorador API_VIEW in funtion

--- apps/users/api/api.py
+++ apps/users/api/api.py
@@ -1,7 +1,8 @@
-from rest_framework.views import APIView
-from apps.users.api.serializers import UserSerializer
-from apps.users.api.serializers import User
 from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+from apps.users.api.serializers import User
+from apps.users.api.serializers import UserSerializer
 
 
 class UserAPIView(APIView):
@@ -10,3 +11,13 @@ class UserAPIView(APIView):
         user = User.objects.all()
         users_serializer = UserSerializer(user, many=True)
         return Response(users_serializer.data)
+
+
+@api_view(['GET', 'POST'])
+def user_api_view(request):
+    if request.method == 'GET':
+        user = User.objects.all()
+        users_serializer = UserSerializer(user, many=True)
+        return Response(users_serializer.data)
+    elif request.method == 'POST':
+        print(request.data)
--- apps/users/api/urls.py
+++ apps/users/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from apps.users.api.api import UserAPIView
+from apps.users.api.api import UserAPIView, user_api_view
 
 urlpatterns = [
-    path('usuario', UserAPIView.as_view(), name='usuario_api')
+    path('usuario', UserAPIView.as_view(), name='usuario_api'),
+    path('api_funtion', user_api_view, name='usuario_api_function'),
 
 ]
