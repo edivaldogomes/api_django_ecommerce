8174c1d1f1a89c202e5089a25e11665bd556f442
Edivaldo Gomes <edslavianov@gmail.com>
2022-09-26 16:24:32 +0200

    Add Detail,Update and Delect with api View

--- apps/users/api/urls.py
+++ apps/users/api/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from apps.users.api.api import UserAPIView, user_api_view
+from apps.users.api.api import UserAPIView, user_api_view, user_detail_view
 
 urlpatterns = [
     path('usuario', UserAPIView.as_view(), name='usuario_api'),
     path('api_funtion', user_api_view, name='usuario_api_function'),
-
+    path('usuario/api_detail_funtion/<int:pk>/', user_detail_view, name='usuario_api_detail_function'),
 ]
